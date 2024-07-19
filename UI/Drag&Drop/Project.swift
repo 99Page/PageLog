9b8b414e0941fc49af514bbcae97fcd474aad5d8
page <nou0ggid@gmail.com>
2024-07-19 14:43:51 +0900

    chore: Drag&Drop Project init

---
+++ UI/Drag&Drop/Project.swift
@@ -0,0 +1,36 @@
+//
+//  Project.swift
+//  TooltipManifests
+//
+//  Created by wooyoung on 7/19/24.
+//
+
+import ProjectDescription
+
+let projectName = "DragAndDrop"
+
+let infoPlist: [String: Plist.Value] = [
+    "CFBundleVersion": "1",
+    "UILaunchStoryboardName": "Launch Screen",
+    "ITSAppUsesNonExemptEncryption": false,
+    "UIUserInterfaceStyle": "Light", // 다크 모드를 사용하지 않습니다.
+]
+
+
+let dragAndDropTarget = Target.target(
+    name: projectName,
+    destinations: .iOS,
+    product: .app,
+    bundleId: "com.page.drag.and.drop",
+    deploymentTargets: .iOS("17.4"),
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: ["Sources/**"],
+    resources: ["../PageComponent/Resources/**"]
+)
+
+let dragAndDropProject = Project(
+    name: projectName,
+    organizationName: "Page",
+    targets: [dragAndDropTarget]
+)
+
