d0cc4f97b7c5c2a94c3c5edfdc9233737899c3ae
99Page <nou0ggid@gmail.com>
2024-08-09 20:04:54 +0900

    chore: 빠진 프로젝트 파일 추가

---
+++ SwiftUI/ApplePhotosEffect/Project.swift
@@ -0,0 +1,35 @@
+//
+//  ApplePhotosEffect.swift
+//  ConcurrencyManifests
+//
+//  Created by 노우영 on 8/9/24.
+//
+
+import ProjectDescription
+
+let projectName = "ApplePhotosEffect"
+
+let infoPlist: [String: Plist.Value] = [
+    "CFBundleVersion": "1",
+    "UILaunchStoryboardName": "Launch Screen",
+    "ITSAppUsesNonExemptEncryption": false,
+    "UIUserInterfaceStyle": "Light", // 다크 모드를 사용하지 않습니다.
+]
+
+
+let target = Target.target(
+    name: projectName,
+    destinations: .iOS,
+    product: .app,
+    bundleId: "com.page.apple.photos.effect",
+    deploymentTargets: .iOS("17.4"),
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: ["Sources/**"],
+    resources: ["../PageComponent/Resources/**"]
+)
+
+let project = Project(
+    name: projectName,
+    organizationName: "Page",
+    targets: [target]
+)
