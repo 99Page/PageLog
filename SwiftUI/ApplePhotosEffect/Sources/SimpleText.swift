c6ef5030e57262eb10ec06bebd5491c01b92e354
99Page <nou0ggid@gmail.com>
2024-08-09 20:04:35 +0900

    feat: 비슷하게 구현 후 임시 저장

---
+++ SwiftUI/ApplePhotosEffect/Sources/SimpleText.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleText.swift
+//  ApplePhotosEffect
+//
+//  Created by 노우영 on 8/9/24.
+//  Copyright © 2024 Page. All rights reserved.
+//
+
+import SwiftUI
+
+struct SimpleText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimpleText()
+}
