d32eedc11e32d7212f105efd585316164a6740e2
99Page <nou0ggid@gmail.com>
2024-08-19 21:27:42 +0900

    solve: 1744 -Heap 코드 개선

---
+++ Algorithm/Baekjoon/no.1744/Project.swift
@@ -0,0 +1,28 @@
+//
+//  Project.swift
+//  ApplePhotosEffectManifests
+//
+//  Created by 노우영 on 8/19/24.
+//
+
+import ProjectDescription
+
+let problemNumber = "no.1744"
+let target = Target.target(
+    name: problemNumber,
+    destinations: .macOS,
+    product: .commandLineTool,
+    bundleId: "com.page.backjoon.\(problemNumber)",
+    deploymentTargets: .macOS("14.5"),
+    sources: ["Sources/**"]
+)
+
+let project = Project(
+    name: "\(problemNumber)",
+    organizationName: "Page",
+    targets: [target]
+)
+
+
+
+
