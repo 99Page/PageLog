0a02bfd1d652983ff4957501e7e6974fbacf6fb1
99Page <nou0ggid@gmail.com>
2024-08-24 10:50:45 +0900

    feat: TCA Scrumdinger 비교를 위한 MV 스타일 만드는 중

---
+++ SwiftUI/PageScrumdinger/Sources/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  ApplePhotosEffectManifests
+//
+//  Created by 노우영 on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StandupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
---
+++ SwiftUI/PageScrumdinger/Tests/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  ApplePhotosEffectManifests
+//
+//  Created by 노우영 on 8/24/24.
+//
+
+import Foundation
