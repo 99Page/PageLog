4188726a669cbb2c0660cd18f60c23c1160c9e5f
Wooyoung Noh <nou0ggid@gmail.com>
2024-08-27 16:08:15 +0900

    Algorithm/baekjoon g3 (#24)
    * solve: 17135
    -Backtracking 두번 사용하는 문제
    
    * feat: Coordinate 기능 추가
    - 유효한 범위만 제공하도록 변경

--- Algorithm/Baekjoon/no.17135/Sources/main.swift
+++ Algorithm/Baekjoon/no.17135/Sources/main.swift
@@ -6,3 +6,216 @@
 //
 
 import Foundation
+
+let archerCount = 3
+let (rowSize, colSize, attackDistance) = readConditions()
+let map = readMap()
+
+printMaxKill()
+
+func printMaxKill() {
+    var maxKill = 0
+    var archerPositionQueue = Queue<ArcherPositions>()
+    let initialArcherPosition: ArcherPositions = .initialPositions
+    archerPositionQueue.enqueue(initialArcherPosition)
+    
+    while !archerPositionQueue.isEmpty {
+        let currentArcherPosition = archerPositionQueue.dequeue()!
+        
+        let killCount = findMaxKill(inCase: currentArcherPosition)
+        maxKill = max(maxKill, killCount)
+        
+        let nextArcherPosition = currentArcherPosition.makeNextPosition(colSize: colSize)
+        
+        if nextArcherPosition != nil {
+            archerPositionQueue.enqueue(nextArcherPosition!)
+        }
+    }
+    
+    
+    print(maxKill)
+}
+
+func findMaxKill(inCase archerPositions: ArcherPositions) -> Int {
+    var killLog: [[Int]] = map
+    var totalKill = 0
+    
+    (1...rowSize).forEach { turn in
+        var nearestEnemies: [Coordinate?] = []
+        
+        (0..<archerCount).forEach { archerNumber in
+            let nearestEnemyFromArcher = findNearestEnenmy(
+                archerPositinons: archerPositions,
+                archerNumber: archerNumber, 
+                turn: turn,
+                killLog: killLog
+            )
+            
+            nearestEnemies.append(nearestEnemyFromArcher)
+        }
+        
+        let killCountInTurn = killNearestEnemies(killLog: &killLog, nearestEnemies: nearestEnemies)
+        totalKill += killCountInTurn
+    }
+    
+    return totalKill
+}
+
+func killNearestEnemies(killLog: inout [[Int]], nearestEnemies: [Coordinate?]) -> Int {
+    var killCount: Int = 0
+    
+    for nearestEnemy in nearestEnemies {
+        if nearestEnemy != nil && killLog[nearestEnemy!.row][nearestEnemy!.col] == 1 {
+            killCount += 1
+            killLog[nearestEnemy!.row][nearestEnemy!.col] = 0
+        }
+    }
+    
+    return killCount
+}
+
+func findNearestEnenmy(archerPositinons: ArcherPositions, archerNumber: Int, turn: Int, killLog: [[Int]]) -> Coordinate? {
+    var searchQueue = Queue<Coordinate>()
+    let isVisitedCol: [Bool] = Array(repeating: false, count: colSize)
+    var isVisitedMap: [[Bool]] = Array(repeating: isVisitedCol, count: rowSize)
+    var nearestEnemy: Coordinate?
+    
+    let nearestCoordinate = archerPositinons.findNearestCoordinate(fromArcher: archerNumber,
+                                                                   turn: turn,
+                                                                   rowSize: rowSize)
+    searchQueue.enqueue(nearestCoordinate)
+    
+    isVisitedMap[nearestCoordinate.row][nearestCoordinate.col] = true
+    
+    if killLog[nearestCoordinate.row][nearestCoordinate.col] == 1 {
+        nearestEnemy = nearestCoordinate
+    }
+    
+    while !(searchQueue.isEmpty || nearestEnemy != nil) {
+        let currentCoordinate = searchQueue.dequeue()!
+        
+        let nextCoordinates = currentCoordinate.nextCoordinates
+        
+        for nextCoordinate in nextCoordinates {
+            if isEnqueuePossible(coordinate: nextCoordinate, isVisited: isVisitedMap) {
+                searchQueue.enqueue(nextCoordinate)
+                isVisitedMap[nextCoordinate.row][nextCoordinate.col] = true
+                
+                if killLog[nextCoordinate.row][nextCoordinate.col] == 1 && nearestEnemy == nil {
+                    nearestEnemy = nextCoordinate
+                }
+            }
+        }
+    }
+    
+    
+    return nearestEnemy
+}
+
+func isEnqueuePossible(coordinate: Coordinate, isVisited: [[Bool]]) -> Bool {
+    isValidCoordinate(coordinate: coordinate) && !isVisited[coordinate.row][coordinate.col]
+    && coordinate.distanceFromArcher <= attackDistance
+}
+
+func isValidCoordinate(coordinate: Coordinate) -> Bool {
+    coordinate.row >= 0 && coordinate.row < rowSize
+    && coordinate.col >= 0 && coordinate.col < colSize
+}
+
+
+struct Coordinate {
+    let row: Int
+    let col: Int
+    let distanceFromArcher: Int
+    
+    var nextCoordinates: [Coordinate] {
+        let left = Coordinate(row: row, col: col - 1, distanceFromArcher: distanceFromArcher + 1)
+        let up = Coordinate(row: row - 1, col: col, distanceFromArcher: distanceFromArcher + 1)
+        let right = Coordinate(row: row, col: col + 1, distanceFromArcher: distanceFromArcher + 1)
+        
+        return [left, up, right]
+    }
+}
+
+
+struct ArcherPositions {
+    let positions: [Int]
+    
+    static var initialPositions: ArcherPositions {
+        ArcherPositions(positions: [0, 1, 2])
+    }
+    
+    /// - Returns: 궁수들의 새로운 위치를 반환합니다. 구할 수 없는 경우 nil입니다.
+    func makeNextPosition(colSize: Int) -> ArcherPositions? {
+        if positions[2] < colSize - 1 {
+            let newThirdArcherPosition = positions[2] + 1
+            return ArcherPositions(positions: [positions[0], positions[1], newThirdArcherPosition])
+        } else if positions[1] < colSize - 2 {
+            let newSecondArcherPosition = positions[1] + 1
+            let newThirdArcherPosition = positions[1] + 2
+            return ArcherPositions(positions: [positions[0], newSecondArcherPosition, newThirdArcherPosition])
+        } else if positions[0] < colSize - 3 {
+            let newFirstArcherPosition = positions[0] + 1
+            let newSecondArcherPosition = positions[0] + 2
+            let newThirdArcherPosition = positions[0] + 3
+            return ArcherPositions(positions: [newFirstArcherPosition, newSecondArcherPosition, newThirdArcherPosition])
+        } else {
+            return nil
+        }
+    }
+    
+    func findNearestCoordinate(fromArcher archer: Int, turn: Int, rowSize: Int) -> Coordinate {
+        let archerColumn = self.positions[archer]
+        
+        return Coordinate(row: rowSize - turn, col: archerColumn, distanceFromArcher: 1)
+        
+    }
+}
+
+
+/// - Returns: 초기 적이 위치 정보가 담긴 배열. 1인 경우 적이 있고, 0인 경우는 없음.
+func readMap() -> [[Int]] {
+    var map: [[Int]] = []
+    
+    (0..<rowSize).forEach { _ in
+        let rowInput = readLine()!.split(separator: " ")
+        let rowInformation = rowInput.map { Int($0)! }
+        map.append(rowInformation)
+    }
+    
+    return map
+}
+
+/// - Returns: 행의 크기, 열의 크기, 궁수의 공격 거리
+func readConditions() -> (Int, Int, Int) {
+    let input = readLine()!
+    let splitedInput = input.split(separator: " ")
+    
+    let n = Int(splitedInput[0])!
+    let m = Int(splitedInput[1])!
+    let d = Int(splitedInput[2])!
+    
+    return (n, m, d)
+}
+
+struct Queue<Element> {
+    private var inStack = [Element]()
+    private var outStack = [Element]()
+    
+    var isEmpty: Bool {
+        inStack.isEmpty && outStack.isEmpty
+    }
+    
+    mutating func enqueue(_ newElement: Element) {
+        inStack.append(newElement)
+    }
+    
+    mutating func dequeue() -> Element? {
+        if outStack.isEmpty {
+            outStack = inStack.reversed()
+            inStack.removeAll()
+        }
+        
+        return outStack.popLast()
+    }
+}
--- Algorithm/Collection/Sources/Coordinate.swift
+++ Algorithm/Collection/Sources/Coordinate.swift
@@ -11,18 +11,35 @@ import Foundation
 struct Coordinate {
     let row: Int
     let col: Int
+    let distance: Int
     
-    /// 각 방향으로의 이동을 나타내는 좌표 목록
-    static let dists: [Coordinate] = [
-        right, left, up, down
-    ]
+    /// 현재 위치에서 이동할 수 있는 상, 하, 좌, 우의 다음 위치를 반환합니다.
+    /// 반환되는 위치들은 0...rowSize-1, 0...colSize-1의 유효 범위 내에 있어야 합니다.
+    ///
+    /// - Parameters:
+    ///   - rowSize: 그리드의 행 크기입니다.
+    ///   - colSize: 그리드의 열 크기입니다.
+    /// - Returns: 상, 하, 좌, 우로 이동한 유효한 좌표들을 반환합니다.
+    func getNextCoordinates(rowSize: Int, colSize: Int) -> [Coordinate] {
+        var nextCoordinates: [Coordinate] = []
+        
+        let left = Coordinate(row: row, col: col - 1, distance: distance + 1)
+        let right = Coordinate(row: row, col: col + 1, distance: distance + 1)
+        let up = Coordinate(row: row - 1, col: col, distance: distance + 1)
+        let down = Coordinate(row: row + 1, col: col, distance: distance + 1)
+        
+        let candidates = [left, right, up, down]
+        
+        for candidate in candidates {
+            if candidate.isValidCoordinate(rowSize: rowSize, colSize: colSize) {
+                nextCoordinates.append(candidate)
+            }
+        }
+        
+        return nextCoordinates
+    }
     
-    /// 오른쪽으로 이동하는 좌표
-    private static let right = Coordinate(row: 0, col: 1)
-    /// 왼쪽으로 이동하는 좌표
-    private static let left = Coordinate(row: 0, col: -1)
-    /// 위로 이동하는 좌표
-    private static let up = Coordinate(row: 1, col: 0)
-    /// 아래로 이동하는 좌표
-    private static let down = Coordinate(row: -1, col: 0)
+    private func isValidCoordinate(rowSize: Int, colSize: Int) -> Bool {
+        self.row >= 0 && self.row < rowSize && self.col >= 0 && self.col < colSize
+    }
 }
