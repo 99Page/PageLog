e4761d54e649da6e3c42c71ae8c9dc88ebd2e312
99Page <nou0ggid@gmail.com>
2024-08-28 10:38:21 +0900

    solve: 2473

--- Algorithm/Baekjoon/no.2473/Sources/main.swift
+++ Algorithm/Baekjoon/no.2473/Sources/main.swift
@@ -9,13 +9,6 @@ import Foundation
 
 /// 합이 가장 작은 것을 어떻게 찾을 것인가??
 /// 모든 경우의 수를 볼 수는 없다.
-///
-/// 투 포인터 + 나머지 한개는 중간지점으로 잡고 여기서 그리디하게 움직이기.
-/// 여기에 예외를 찾아볼까.
-/// 언제가 예외일까?
-/// 중간 지점에서 한쪽 방향으로 움직이는 건 예외 상황없다.
-/// 투 포인터에 있는 양쪽 값에 따라 값을 더하든 빼든 한쪽 방향으로만 움직이면 되니까.
-/// 투 포인터를 움직였을 때 예외 상황인 경우가 없다면 맞는 솔루션일거 같다.
 
 let solutionCount = Int(readLine()!)!
 let solutions = readSolutions()
@@ -23,53 +16,87 @@ let solutions = readSolutions()
 solve()
 
 func solve() {
-    var leftPoint = 0
-    var rightPoint = solutionCount - 1
     
-    var selectedSolutionsIndex: [Int] = []
-    var minSum: Int = .max
+    var finalSolutionSum = SolutionSum(absSum: .max, selectedSolutionIndexes: [0, 0, 0])
     
-    while !(rightPoint - leftPoint < 1) {
-        let (min, middlePoint) = find(leftPoint: leftPoint, rightPoint: rightPoint)
+    for fixedSolutionPoint in (0..<solutionCount) {
+        let solutionSum = findMinAbsoluteSum(with: fixedSolutionPoint)
         
-        if min < minSum {
-            minSum = min
-            selectedSolutionsIndex = [leftPoint, middlePoint, rightPoint]
+        if solutionSum.absSum < finalSolutionSum.absSum {
+            finalSolutionSum = solutionSum
         }
         
-        if solutions[leftPoint] + solutions[rightPoint] > 0 {
+        if finalSolutionSum.absSum == 0 {
+            break
+        }
+    }
+    
+    let sortedIndex = finalSolutionSum.selectedSolutionIndexes.sorted()
+    let firstSolution = solutions[sortedIndex[0]]
+    let secondSolution = solutions[sortedIndex[1]]
+    let thirdSolution = solutions[sortedIndex[2]]
+    
+    print(firstSolution, secondSolution, thirdSolution)
+}
+
+func findMinAbsoluteSum(with fixedSolutionPoint: Int) -> SolutionSum {
+    var leftPoint = fixedSolutionPoint + 1
+    var rightPoint = solutionCount - 1
+    
+    let selectedSolutionIndexes: [Int] = [fixedSolutionPoint, leftPoint, rightPoint]
+    var solutionSum = SolutionSum(absSum: .max, selectedSolutionIndexes: selectedSolutionIndexes)
+    
+    avoidFixedSolutionPoint(
+        fixedSolutionPoint: fixedSolutionPoint,
+        leftPoint: &leftPoint,
+        rightPoint: &rightPoint
+    )
+    
+    let fixedSolution = solutions[fixedSolutionPoint]
+    
+    while leftPoint < rightPoint {
+        let twoSolutionsSum = solutions[leftPoint] + solutions[rightPoint]
+        
+        let threeSolutionsSum = twoSolutionsSum + fixedSolution
+        
+        if abs(threeSolutionsSum) < solutionSum.absSum {
+            let newSelectedIndexes = [fixedSolutionPoint, leftPoint, rightPoint]
+            solutionSum = SolutionSum(absSum: abs(threeSolutionsSum), selectedSolutionIndexes: newSelectedIndexes)
+        }
+        
+        if solutionSum.absSum == 0 {
+            break
+        }
+        
+        if threeSolutionsSum > 0 {
             rightPoint -= 1
         } else {
             leftPoint += 1
         }
+        
+        avoidFixedSolutionPoint(
+            fixedSolutionPoint: fixedSolutionPoint,
+            leftPoint: &leftPoint,
+            rightPoint: &rightPoint
+        )
     }
     
-    print("\(solutions[selectedSolutionsIndex[0]]) \(solutions[selectedSolutionsIndex[1]]) \(solutions[selectedSolutionsIndex[2]])")
+    return solutionSum
 }
 
-func find(leftPoint: Int, rightPoint: Int) -> (Int, Int) {
-    var finalMiddelPoint = -1
-    var middlePoint = (leftPoint + rightPoint) / 2
-    var sumOfThreeSolution: Int = .max
-    
-    while middlePoint > leftPoint && middlePoint < rightPoint {
-        let newSum = solutions[leftPoint] + solutions[middlePoint] + solutions[rightPoint]
-        
-        let absoluteNewSum = abs(newSum)
-        
-        if absoluteNewSum < sumOfThreeSolution {
-            finalMiddelPoint = middlePoint
-            sumOfThreeSolution = absoluteNewSum
-        }
-        
-        if newSum < 0 {
-            middlePoint += 1
-        } else {
-            middlePoint -= 1
-        }
+func avoidFixedSolutionPoint(fixedSolutionPoint: Int, leftPoint: inout Int, rightPoint: inout Int) {
+    if fixedSolutionPoint == leftPoint {
+        leftPoint += 1
     }
     
-    return (sumOfThreeSolution, finalMiddelPoint)
+    if fixedSolutionPoint == rightPoint {
+        rightPoint -= 1
+    }
+}
+
+struct SolutionSum {
+    let absSum: Int
+    let selectedSolutionIndexes: [Int]
 }
 
 func readSolutions() -> [Int] {
