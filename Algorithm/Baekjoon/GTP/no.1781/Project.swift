71b8c285881decf1361de5cfbadfa3a9582824c2
99Page <nou0ggid@gmail.com>
2024-09-13 12:30:59 +0900

    solve: 1781 -greedy + 빠른 배열 순회

---
+++ Algorithm/Baekjoon/GTP/no.1781/Project.swift
@@ -0,0 +1,23 @@
+import ProjectDescription
+
+let problemNumber = "1781"
+let target = Target.target(
+    name: problemNumber,
+    destinations: .macOS,
+    product: .commandLineTool,
+    bundleId: "com.page.backjoon.\(problemNumber)",
+    deploymentTargets: .macOS("14.5"),
+    sources: ["Sources/**"]
+)
+
+let project = Project(
+    name: "\(problemNumber)",
+    organizationName: "Page",
+    targets: [target]
+)
+
+
+
+
+
+
---
+++ Algorithm/Baekjoon/GTP/no.1781/Sources/main.swift
@@ -0,0 +1,235 @@
+//
+//  main.swift
+//  2023KakaoManifests
+//
+//  Created by 노우영 on 9/13/24.
+//
+
+import Foundation
+
+
+/// Greedy하게는 불가능하다
+/// 시간이 빠른거대로 먹든
+/// 보상이 큰거대로 먹든 반례는 존재한다.
+//
+//2
+//1 10
+//3 11
+//결과: 21
+
+//7
+//1 6
+//1 7
+//3 2
+//3 1
+//2 4
+//2 5
+//6 1
+
+
+//3
+//1 5
+//2 10
+//2 11
+//결과: 21
+
+
+//4
+//4 10
+//4 5
+//4 4
+//4 3
+//결과: 22
+
+let homeworkCount = Int(readLine()!)!
+var homeworks: [Homework] = []
+var nearestEmptyDeadline: [Int] = []
+var noddles: [Int] = []
+
+solve()
+
+func solve() {
+    readInputs()
+    findMaxNoodleCount()
+    
+    let sum = noddles.reduce(into: 0) { partialResult, value in
+        partialResult += value
+    }
+    
+    print(sum)
+}
+
+/// 받을 수 있는 최대 컵라면 개수를 구합니다.
+/// - Returns: 최대 컵라면 개수
+func findMaxNoodleCount() {
+    var homeworkPQ = Heap<Homework>.maxHeap()
+    
+    for homework in homeworks {
+        homeworkPQ.insert(homework)
+    }
+    
+    while !homeworkPQ.isEmpty {
+        let homework = homeworkPQ.pop()!
+        
+        fillNoodle(to: homework.deadline, noddleCount: homework.noddleCount)
+    }
+}
+
+func fillNoodle(to deadline: Int, noddleCount: Int) -> Int {
+    guard deadline != 0 else { return 0 }
+    
+    if noddles[deadline] == 0 {
+        noddles[deadline] = noddleCount
+        return nearestEmptyDeadline[deadline]
+    } else {
+        let emptyDeadline = nearestEmptyDeadline[deadline]
+        let find = fillNoodle(to: emptyDeadline, noddleCount: noddleCount)
+        nearestEmptyDeadline[deadline] = find
+        return find
+    }
+}
+
+/// 숙제의 정보를 입력받습니다.
+func readInputs() {
+    var maxDeadLine = 0
+    
+    (0..<homeworkCount).forEach { _ in
+        let input: [Int] = readArray()
+        let homework = Homework(deadline: input[0], noddleCount: input[1])
+        homeworks.append(homework)
+        maxDeadLine = max(maxDeadLine, input[0])
+    }
+    
+    nearestEmptyDeadline.append(0)
+    
+    for index in 1...maxDeadLine {
+        nearestEmptyDeadline.append(index - 1)
+    }
+
+    
+    noddles = Array(repeating: 0, count: maxDeadLine + 1)
+}
+
+func readArray<T: LosslessStringConvertible>() -> [T] {
+    let line = readLine()!
+    let splitedLine = line.split(separator: " ")
+    let array = splitedLine.map { T(String($0))! }
+    return array
+}
+
+
+struct Homework: Comparable {
+    static func < (lhs: Homework, rhs: Homework) -> Bool {
+        if lhs.noddleCount != rhs.noddleCount {
+            return lhs.noddleCount < rhs.noddleCount
+        } else {
+            return lhs.deadline < rhs.deadline
+        }
+    }
+    
+    let deadline: Int
+    let noddleCount: Int
+}
+
+struct Heap<Element: Comparable> {
+    private let comparator: (Element, Element) -> Bool
+    private(set) var storage: [Element] = []
+    
+    init(comparator: @escaping (Element, Element) -> Bool) {
+        self.comparator = comparator
+    }
+    
+    var isEmpty: Bool {
+        storage.isEmpty
+    }
+    
+    func peek() -> Element? {
+        storage.first
+    }
+    
+    /// - Complexity: O(log n)
+    mutating func insert(_ element: Element) {
+        storage.append(element)
+        let insertedIndex = storage.count - 1
+        siftUp(startIndex: insertedIndex)
+    }
+    
+    mutating private func siftUp(startIndex: Int) {
+        var currentIndex = startIndex
+        
+        while hasHigherPriorityThanParent(index: currentIndex) {
+            let parentIndex = parentIndex(childIndex: currentIndex)
+            storage.swapAt(currentIndex, parentIndex)
+            currentIndex = parentIndex
+        }
+    }
+    
+    
+    private func hasHigherPriorityThanParent(index: Int) -> Bool {
+        guard index < storage.endIndex else { return false }
+        
+        let parentIndex = parentIndex(childIndex: index)
+        let parentElement = storage[parentIndex]
+        let childElement = storage[index]
+        
+        return comparator(childElement, parentElement)
+    }
+    
+    mutating func pop() -> Element? {
+        guard !storage.isEmpty else { return nil }
+        
+        guard storage.count != 1 else { return storage.removeLast()}
+        
+        let lastIndex = storage.count - 1
+        
+        storage.swapAt(0, lastIndex)
+        let result = storage.removeLast()
+        
+        siftDown(currentIndex: 0)
+        
+        return result
+    }
+    
+    private mutating func siftDown(currentIndex: Int) {
+        var swapIndex = currentIndex
+        var isSwap = false
+        let leftIndex = leftChildIndex(parentIndex: currentIndex)
+        let rightIndex = rightChildIndex(parentIndex: currentIndex)
+        
+        if hasHigherPriorityThanParent(index: leftIndex) {
+            swapIndex = leftIndex
+            isSwap = true
+        }
+        
+        if rightIndex < storage.endIndex && comparator(storage[rightIndex], storage[swapIndex]) {
+            swapIndex = rightIndex
+            isSwap = true
+        }
+        
+        if isSwap {
+            storage.swapAt(swapIndex, currentIndex)
+            siftDown(currentIndex: swapIndex)
+        }
+    }
+    
+    static func minHeap() -> Self{
+        self.init(comparator: <)
+    }
+    
+    static func maxHeap() -> Self{
+        self.init(comparator: >)
+    }
+    
+    // MARK: Index 관련 함수들
+    private func leftChildIndex(parentIndex: Int) -> Int {
+        parentIndex * 2 + 1
+    }
+    
+    private func rightChildIndex(parentIndex: Int) -> Int {
+        parentIndex * 2 + 2
+    }
+    
+    private func parentIndex(childIndex: Int) -> Int {
+        (childIndex - 1) / 2
+    }
+}
