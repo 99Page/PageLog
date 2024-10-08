21ff781fac79357124a102adb489bc2e7f2b10b7
99Page <nou0ggid@gmail.com>
2024-10-08 21:12:41 +0900

    chore: 단일한 백준 프로젝트로 통합

--- Algorithm/Baekjoon/Records/main/Project.swift
+++ Algorithm/Baekjoon/Records/main/Project.swift
@@ -1,11 +1,11 @@
 import ProjectDescription
 
-let problemNumber = "gold-platinum-solver"
+let problemNumber = "baekjoon-solve"
 let target = Target.target(
     name: problemNumber,
     destinations: .macOS,
     product: .commandLineTool,
-    bundleId: "com.page.backjoon.gold.platinum.solver",
+    bundleId: "com.page.backjoon.solver",
     deploymentTargets: .macOS("14.5"),
     sources: ["Sources/**"]
 )
