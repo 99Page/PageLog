35bebcd6793d63bb5c6b04cf7df74e9a4261cc79
page <nou0ggid@gmail.com>
2025-02-14 08:48:42 +0900

    feat: uibinding

---
+++ UIKit/Sources/TCA/Binding/ColorBindingView.swift
@@ -0,0 +1,52 @@
+//
+//  ColorBindingView.swift
+//  CaseStudies-UIKit
+//
+//  Created by Reppley_iOS on 2/14/25.
+//  Copyright © 2025 Page. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+import ComposableArchitecture
+
+@Observable
+class ColorState {
+    var color: UIColor = .red
+}
+
+class ColorBindingView: UIView {
+    @UIBinding var color: UIColor
+    
+    init(color: UIBinding<UIColor>) {
+        self._color = color
+        super.init(frame: .zero)
+        
+        updateView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func updateView() {
+        observe { [weak self] in
+            guard let self else { return }
+            self.backgroundColor = color
+        }
+        
+        
+    }
+}
+
+#Preview {
+    @Previewable @UIBindable var color = ColorState()
+
+    UIViewPreview {
+        ColorBindingView(color: $color.color)
+    }
+    .onTapGesture {
+        debugPrint("tap")
+        color.color = .green
+    }
+}
