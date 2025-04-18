cd25b1fc66ba30c918b24bd40037db700022f501
page <nou0ggid@gmail.com>
2025-04-18 15:30:24 +0900

    doc: pagination timing

---
+++ TIL/Project.swift
@@ -0,0 +1,35 @@
+//
+//  Project.swift
+//  2023KakaoManifests
+//
+//  Created by Reppley_iOS on 4/18/25.
+//
+
+import Foundation
+import ProjectDescription
+
+let projectName = "TIL"
+
+let infoPlist: [String: Plist.Value] = [
+    "CFBundleVersion": "1",
+    "UILaunchStoryboardName": "Launch Screen",
+    "ITSAppUsesNonExemptEncryption": false,
+    "UIUserInterfaceStyle": "Light", // 다크 모드를 사용하지 않습니다.
+]
+
+
+let target = Target.target(
+    name: projectName,
+    destinations: .iOS,
+    product: .app,
+    bundleId: "com.page.today.i.learned",
+    deploymentTargets: .iOS("18.0"),
+    infoPlist: .extendingDefault(with: infoPlist),
+    sources: ["Sources/**"]
+)
+
+let project = Project(
+    name: projectName,
+    organizationName: "Page",
+    targets: [target]
+)
