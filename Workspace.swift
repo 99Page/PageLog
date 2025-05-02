fdc007baaa87393543c65cee0b0245f246d4aed5
노우영 <nou0ggid@gmail.com>
2025-05-02 15:59:02 +0900

    2025-04 개발 경험 정리

--- Workspace.swift
+++ Workspace.swift
@@ -9,5 +9,5 @@ import ProjectDescription
 
 let workspace = Workspace(
     name: "PageLog",
-    projects: ["UI/**", "Language/**", "Algorithm/**", "TCA/**", "Resource/**", "TIL/**", "PageKit/**"]
+    projects: ["UI/**", "Language/**", "Algorithm/**", "TCA/**", "Resource/**", "JustDoc/**", "PageKit/**"]
 )
