11d7b3d3e4096745747d5e2855a0f306b50f72ec
노우영 <nou0ggid@gmail.com>
2025-05-19 19:00:12 +0900

    doc: category

--- Algorithm/Baekjoon/Records/main/Sources/main.swift
+++ Algorithm/Baekjoon/Records/main/Sources/main.swift
@@ -2,74 +2,172 @@
 var solver = Solver()
 solver.solve()
 
-//3
-//2 Z ABC
-//2 ABC Z
-//2 ZZ ABC
-
-//5
-//1 C
-//2 A B
-//3 A A A
-//2 B A
-//2 A C
-
-//4
-//2 APPLE BANANA
-//4 APPLE APPLE BANANA BANANA
-//3 APPLE APPLE KIWI
-//3 APPLE APPLE APPLE
-
-//3
-//1 APPLE
-//1 BANANA
-//1 KIWI
 struct Solver {
-    let feeds: [String]
-    let feedCount: Int
+    
+    let rowSize: Int
+    let columnSize: Int
+    
+    var map: [[Character]]
+    var mem: [[Int]]
+    let maxVisit = 50 * 50
     
     init() {
-        self.feedCount = Int(readLine()!)!
+        let gridSize: [Int] = readArray()
+        self.rowSize = gridSize[0]
+        self.columnSize = gridSize[1]
         
-        var feeds: [String] = []
+        self.map = []
+        self.mem = []
         
-        (0..<feedCount).forEach { _ in
-            var feed = readLine()!.split(separator: " ")
-            feed.removeFirst()
-            let feedString = feed.reduce(into: "", { $0 += String($1 + " ") })
-            feeds.append(feedString)
+        (0..<rowSize).forEach { _ in
+            map.append(Array(readLine()!))
         }
         
-        self.feeds = feeds
+        self.mem = map.map { $0.map { _ in 0 } }
     }
     
     mutating func solve() {
-        let sortedFeeds = feeds.sorted()
-        var oldFeed: [String] = Array(repeating: "", count: 15)
-        var result: [String] = []
+        let start = Coordinate(row: 0, col: 0, distance: 0)
         
-        for feed in sortedFeeds {
-            let newFeed = feed.split(separator: " ")
-            var prefix = ""
-            
-            for index in newFeed.indices {
-                if oldFeed[index] != newFeed[index] {
-                    result.append("\(prefix)\(newFeed[index])")
-                }
-                
-                prefix += "--"
+        var visitMap = map.map { $0.map { _ in false } }
+        
+        visitMap[0][0] = true
+        let result = visit(start, visitMap: &visitMap)
+        print(result == maxVisit ? -1 : result)
+    }
+    
+    mutating func visit(_ coordinate: Coordinate, visitMap: inout [[Bool]]) -> Int {
+        let row = coordinate.row
+        let col = coordinate.col
+        
+        guard map[row][col] != "H" else { return 0 }
+        guard mem[row][col] == 0 else { return min(mem[row][col], maxVisit) }
+        
+        let jumpSize = Int(String(map[row][col]))!
+        let directions = [(jumpSize, 0), (-jumpSize, 0), (0, jumpSize), (0, -jumpSize)]
+        
+        let targets = directions
+            .map { (dr, dc) in
+                Coordinate(row: row + dr, col: col + dc, distance: 0)
             }
-            
-            for index in 0..<15 {
-                if index < newFeed.count {
-                    oldFeed[index] = String(newFeed[index])
-                } else {
-                    oldFeed[index] = ""
-                }
+            .filter { $0.isValidCoordinate(rowSize: rowSize, colSize: columnSize) }
+        
+        var maxCount = 0
+        
+        for target in targets {
+            if visitMap[target.row][target.col] {
+                mem[row][col] = maxVisit
+                return maxVisit
+            } else {
+                visitMap[target.row][target.col] = true
+                maxCount = max(visit(target, visitMap: &visitMap), maxCount)
+                visitMap[target.row][target.col] = false
             }
         }
         
-        let reduced = result.reduce(into: "") { $0 += "\($1)\n" }
-        print(reduced)
+        mem[row][col] = maxCount + 1
+        return min(mem[row][col], maxVisit)
     }
+    
+    struct Coordinate {
+        let row: Int
+        let col: Int
+        let distance: Int
+        let arrayIndexBase: ArrayIndexBase
+        
+        init(row: Int, col: Int, distance: Int, arrayIndexBase: ArrayIndexBase = .zero) {
+            self.row = row
+            self.col = col
+            self.distance = distance
+            self.arrayIndexBase = arrayIndexBase
+        }
+        
+        /// 현재 위치에서 이동할 수 있는 모든 유효한 다음 위치를 반환합니다.
+        /// 반환되는 위치들은 0...rowSize-1, 0...colSize-1의 유효 범위 내에 있어야 합니다.
+        /// 이동 가능한 방향은 상, 하, 좌, 우 및 대각선입니다.
+        ///
+        /// - Parameters:
+        ///   - rowSize: 그리드의 행 크기입니다.
+        ///   - colSize: 그리드의 열 크기입니다.
+        /// - Returns: 모든 방향으로 이동한 유효한 좌표들을 반환합니다.
+        func neighborsIncludingDiagonals(rowSize: Int, colSize: Int) -> [Coordinate] {
+            let directions = [
+                (-1, 0), (1, 0), (0, -1), (0, 1),     // 상, 하, 좌, 우
+                (-1, -1), (-1, 1), (1, -1), (1, 1)    // 대각선
+            ]
+            
+            return directions
+                .map { (dr, dc) in
+                    Coordinate(row: row + dr, col: col + dc, distance: distance + 1, arrayIndexBase: arrayIndexBase)
+                }
+                .filter { $0.isValidCoordinate(rowSize: rowSize, colSize: colSize) }
+        }
+        
+        /// 현재 위치에서 이동할 수 있는 상, 하, 좌, 우의 다음 위치를 반환합니다.
+        /// 반환되는 위치들은 0...rowSize-1, 0...colSize-1의 유효 범위 내에 있어야 합니다.
+        ///
+        /// - Parameters:
+        ///   - rowSize: 그리드의 행 크기입니다.
+        ///   - colSize: 그리드의 열 크기입니다.
+        /// - Returns: 상, 하, 좌, 우로 이동한 유효한 좌표들을 반환합니다.
+        func neighbors(rowSize: Int, colSize: Int) -> [Coordinate] {
+            let directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            
+            return directions
+                .map { (dr, dc) in
+                    Coordinate(row: row + dr, col: col + dc, distance: distance + 1, arrayIndexBase: arrayIndexBase)
+                }
+                .filter { $0.isValidCoordinate(rowSize: rowSize, colSize: colSize) }
+            
+        }
+        
+        /// 주어진 그리드 크기에서 현재 좌표가 유효한지 확인합니다.
+        /// 이 함수는 정사각형 그리드에 대한 유효성을 검사하는 데 사용됩니다.
+        ///
+        /// - Parameter gridSize: 정사각형 그리드의 크기입니다. 행과 열의 크기가 동일합니다.
+        /// - Returns: 현재 좌표가 유효하다면 `true`, 그렇지 않다면 `false`를 반환합니다.
+        func isValidCoordinate(gridSize: Int) -> Bool {
+            isValidCoordinate(rowSize: gridSize, colSize: gridSize)
+        }
+        
+        /// 주어진 행 크기와 열 크기를 기준으로 현재 좌표가 유효한지 확인합니다.
+        /// 이 함수는 직사각형 그리드에 대한 유효성을 검사하는 데 사용됩니다.
+        ///
+        /// - Parameters:
+        ///   - rowSize: 그리드의 행 크기입니다.
+        ///   - colSize: 그리드의 열 크기입니다.
+        /// - Returns: 현재 좌표가 유효하다면 `true`, 그렇지 않다면 `false`를 반환합니다.
+        func isValidCoordinate(rowSize: Int, colSize: Int) -> Bool {
+            let rowThreshold = rowSize + arrayIndexBase.additionalIndexToSize
+            let colThreshold = colSize + arrayIndexBase.additionalIndexToSize
+            
+            return self.row >= arrayIndexBase.minIndex && self.row < rowThreshold
+            && self.col >= arrayIndexBase.minIndex && self.col < colThreshold
+        }
+        
+        enum ArrayIndexBase {
+            case zero
+            case one
+            
+            var minIndex: Int {
+                switch self {
+                case .zero:
+                    return 0
+                case .one:
+                    return 1
+                }
+            }
+            
+            var additionalIndexToSize: Int { self.minIndex }
+        }
+    }
+
 }
+
+private func readArray<T: LosslessStringConvertible>() -> [T] {
+    let line = readLine()!
+    let splitedLine = line.split(separator: " ")
+    let array = splitedLine.map { T(String($0))! }
+    return array
+}
+
