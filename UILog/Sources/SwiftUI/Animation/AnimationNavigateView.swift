9b509df158820c4722335c9e03002127342bfbb2
노우영 <nou0ggid@gmail.com>
2025-11-05 15:39:09 +0900

    리팩토링 실패한 코드 제거

--- UILog/Sources/SwiftUI/Animation/AnimationNavigateView.swift
+++ UILog/Sources/SwiftUI/Animation/AnimationNavigateView.swift
@@ -14,10 +14,6 @@ struct AnimationNavigateView: View {
             NavigationLink("UIViewAnimation") {
                 UIViewAnimationView()
             }
-            
-            NavigationLink("ZoomTransition") {
-                ZoomTransitionView()
-            }
         }
     }
 }
